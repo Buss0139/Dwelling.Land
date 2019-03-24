2a0b97fcb83553c0c7819be56cc85db0a5b9ea89
CBussey <Buss0139@gmail.com>
2019-03-23 20:42:56 -0500

    We Live

---
+++ Server/app.js
@@ -0,0 +1,22 @@
+
+
+console.log("Hello from server.js");
+
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
+app.use( express.static('public') );
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+  console.log('Server up:', app.get('port'));
+});
+
+
+app.get( '/', function( req, res ){
+  console.log( 'Serving Home' );
+  res.sendFile( path.resolve( 'Public/index.html' ) );
+});
+
